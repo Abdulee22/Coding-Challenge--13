40c4f703cea27376bb7a185504125096d83916c5
Abdulhakim <Abdulthe383@gmail.com>
2025-03-09 16:25:12 -0400

    Task 3 - Bulk Update on Employee Cards

--- cc_13.js
+++ cc_13.js
@@ -7,5 +7,13 @@ employeeCard.innerHTML = `
     <h3 class="employee-name">${name}</h3>
     <p class="employee-position">${position}</p>
     <button class="edit-btn">Edit</button>
-    <button class="remove-btn">Remove</button>`; }// Populated the employee card with name, position, and buttons
-    
+    <button class="remove-btn">Remove</button>`}; // Populated the employee card with name, position, and buttons
+
+// Task 3: Bulk Update on Employee Cards //
+
+function highlightEmployeeCards() {
+const employeeCards = document.querySelectorAll(".employee-card"); // all employee cards
+const employeeCardsArray = [...employeeCards];
+employeeCardsArray.forEach(card => { // applyed styling
+card.style.border = "2px solid blue";   // blue
+card.style.backgroundColor = "#f0f8ff";});} // blue
